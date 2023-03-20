52e0617ab5947cc0c51152df0f2c44261f72ab12
tharwat samy <63080646+tharwatsamy@users.noreply.github.com>
2023-03-21 01:20:39 +0200

    book entity

---
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -0,0 +1,9 @@
+class BookEntity {
+  final String image;
+  final String title;
+  final String authorName;
+  final num price;
+  final num rating;
+
+  BookEntity(this.image, this.title, this.authorName, this.price, this.rating);
+}
