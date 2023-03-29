96f14070b0ab21bb7b6e44ae04322b451206cf46
tharwat samy <63080646+tharwatsamy@users.noreply.github.com>
2023-03-29 18:30:55 +0200

    generate book entity adapter

--- lib/Features/home/domain/entities/book_entity.dart
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -1,9 +1,19 @@
+import 'package:hive/hive.dart';
+part 'book_entity.g.dart';
+
+@HiveType(typeId: 0)
 class BookEntity {
+  @HiveField(0)
   final String bookId;
+  @HiveField(1)
   final String? image;
+  @HiveField(2)
   final String title;
+  @HiveField(3)
   final String? authorName;
+  @HiveField(4)
   final num? price;
+  @HiveField(5)
   final num? rating;
 
   BookEntity(
---
+++ lib/Features/home/domain/entities/book_entity.g.dart
@@ -0,0 +1,56 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'book_entity.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class BookEntityAdapter extends TypeAdapter<BookEntity> {
+  @override
+  final int typeId = 0;
+
+  @override
+  BookEntity read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return BookEntity(
+      image: fields[1] as String?,
+      title: fields[2] as String,
+      authorName: fields[3] as String?,
+      price: fields[4] as num?,
+      rating: fields[5] as num?,
+      bookId: fields[0] as String,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, BookEntity obj) {
+    writer
+      ..writeByte(6)
+      ..writeByte(0)
+      ..write(obj.bookId)
+      ..writeByte(1)
+      ..write(obj.image)
+      ..writeByte(2)
+      ..write(obj.title)
+      ..writeByte(3)
+      ..write(obj.authorName)
+      ..writeByte(4)
+      ..write(obj.price)
+      ..writeByte(5)
+      ..write(obj.rating);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is BookEntityAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
