e674152287f9aa8c7a3c1b8e6469ea0d4f6c5b7e
tharwat samy <63080646+tharwatsamy@users.noreply.github.com>
2023-03-29 03:48:27 +0200

    create home local data source

---
+++ lib/Features/home/data/data_sources/home_local_data_source.dart
@@ -0,0 +1,21 @@
+import 'package:bookly/Features/home/domain/entities/book_entity.dart';
+import 'package:flutter/material.dart';
+
+abstract class HomeLocalDataSource {
+  List<BookEntity> fetchFeaturedBooks();
+  List<BookEntity> fetchNewestBooks();
+}
+
+class HomeLocalDataSourceImpl extends HomeLocalDataSource {
+  @override
+  List<BookEntity> fetchFeaturedBooks() {
+    // TODO: implement fetchFeaturedBooks
+    throw UnimplementedError();
+  }
+
+  @override
+  List<BookEntity> fetchNewestBooks() {
+    // TODO: implement fetchNewestBooks
+    throw UnimplementedError();
+  }
+}
