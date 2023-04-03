a3555964fc7334a33dbdd94c1a8a81663ec02dd0
tharwat samy <63080646+tharwatsamy@users.noreply.github.com>
2023-04-04 00:59:48 +0200

    careate featured books states

---
+++ lib/Features/home/presentation/manger/featured_books_cubit/featured_books_cubit.dart
@@ -0,0 +1,9 @@
+import 'package:bloc/bloc.dart';
+import 'package:bookly/Features/home/domain/entities/book_entity.dart';
+import 'package:meta/meta.dart';
+
+part 'featured_books_state.dart';
+
+class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
+  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+}
---
+++ lib/Features/home/presentation/manger/featured_books_cubit/featured_books_state.dart
@@ -0,0 +1,20 @@
+part of 'featured_books_cubit.dart';
+
+@immutable
+abstract class FeaturedBooksState {}
+
+class FeaturedBooksInitial extends FeaturedBooksState {}
+
+class FeaturedBooksLoading extends FeaturedBooksState {}
+
+class FeaturedBooksFailure extends FeaturedBooksState {
+  final String errMessage;
+
+  FeaturedBooksFailure(this.errMessage);
+}
+
+class FeaturedBooksSuccess extends FeaturedBooksState {
+  final List<BookEntity> books;
+
+  FeaturedBooksSuccess(this.books);
+}
