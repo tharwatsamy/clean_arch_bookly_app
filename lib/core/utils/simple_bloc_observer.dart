d78a2055aa1ffe43720b28867bae40a1c610e0cb
tharwat samy <63080646+tharwatsamy@users.noreply.github.com>
2023-04-19 02:11:26 +0200

    create simple bloc observer

---
+++ lib/core/utils/simple_bloc_observer.dart
@@ -0,0 +1,11 @@
+import 'dart:developer';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SimpleBlocObserver extends BlocObserver {
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    log(change.toString());
+    super.onChange(bloc, change);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:bookly/Features/home/presentation/manger/featured_books_cubit/fe
 import 'package:bookly/Features/home/presentation/manger/newset_books_cubit/newest_books_cubit.dart';
 import 'package:bookly/constants.dart';
 import 'package:bookly/core/utils/app_router.dart';
+import 'package:bookly/core/utils/simple_bloc_observer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -19,6 +20,7 @@ void main() async {
   setupServiceLocator();
   await Hive.openBox<BookEntity>(kFeaturedBox);
   await Hive.openBox<BookEntity>(kNewestBox);
+  Bloc.observer = SimpleBlocObserver();
   runApp(const Bookly());
 }
 
